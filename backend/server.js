64b6e4296f75a6406ab24c32e8b7874450f84565
dhruv1rathod <dhruvrathod.24.ds@iict.indusuni.ac.in>
2026-03-15 18:35:02 +0530

    fix api url

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 const express   = require("express")
 const mongoose  = require("mongoose")
 const cors      = require("cors")
+app.use(cors());
 
 const authRoutes      = require("./routes/auth")
 const quotationRoutes = require("./routes/quotations")
