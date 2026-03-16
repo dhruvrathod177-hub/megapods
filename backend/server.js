18c2465db9afbd4106ea39620df5c68aca2f55aa
dhruv1rathod <dhruvrathod.24.ds@iict.indusuni.ac.in>
2026-03-16 15:35:49 +0530

    fix cors for www subdomain

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ app.use(cors({
   origin: [
     "https://megapodsindia.shop",
     "https://www.megapodsindia.shop",
-    "http://localhost:5173"
-    
+    "http://localhost:5173",
+    "http://localhost:5174",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
